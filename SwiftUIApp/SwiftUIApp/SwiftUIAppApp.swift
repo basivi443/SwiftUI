da7a14856587d368ea06a8a01432c418aae7858a
basivireddy <basivi.info016@gmail.com>
2024-12-23 22:27:15 +0530

    Initial commit

---
+++ SwiftUIApp/SwiftUIApp/SwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppApp.swift
+//  SwiftUIApp
+//
+//  Created by Basivi Reddy on 23/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
